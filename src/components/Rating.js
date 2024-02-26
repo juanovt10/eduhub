6cb504a48aa598a08226e227f32d4cc90011df84
Juan Velasquez <juanovelasquez10@gmail.com>
2024-02-26 14:46:48 +0000

    Create ratings component to display ratings in using font awsome stars icons

---
+++ src/components/Rating.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Rating = (props) => {
+    const {
+        rating,
+        ratings_count,
+    } = props
+
+    const fullStar = Math.floor(rating);
+    const halfStar = rating % 1 >= 0.5;
+    const emptyStars = 5 - fullStar - (halfStar ? 1 : 0);
+
+    return (
+        <div>
+            {Array(fullStar).fill().map((_, index) => (
+                <i class="fa-solid fa-star"></i>
+            ))}
+            {halfStar && <i class="fa-solid fa-star-half-stroke"></i>}
+            {Array(emptyStars).fill().map((_, index) => (
+                <i class="fa-regular fa-star"></i>
+            ))}
+            <span className="ml-1">{ratings_count} reviews</span>
+        </div>
+    )
+}
+
+export default Rating
--- src/pages/courses/CourseDetail.js
+++ src/pages/courses/CourseDetail.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useCurrentUser } from '../../context/CurrentUserContext'
 import { Col, Image, Row } from 'react-bootstrap';
 import styles from '../../styles/CourseDetail.module.css'
+import Rating from '../../components/Rating';
 
 const CourseDetail = (props) => {
     const {
@@ -36,11 +37,19 @@ const CourseDetail = (props) => {
                 </Col>
                 <Col className={styles.title}>
                     <Image src={image}/>
+                    <h5 className="mt-2">By: {owner}. Created on: {created_at}</h5>
                 </Col>
             </Row>
             <Row>
+                <Col className={styles.title}>
+                    <p><i class="fa-solid fa-video"></i> {video_hours} total video hours</p>
+                    <p><i class="fa-brands fa-readme"></i> {article_count} total articles</p>
+                    <p><i class="fa-solid fa-pen-to-square"></i> {test_count} total tests</p>
+                    <Rating rating={overall_rating} ratings_count={ratings_count}/>
+                </Col>
                 <Col>
-                    {}
+                    <p>Price: ${price}</p>
+                    <p>Duration: {duration}</p>
                 </Col>
             </Row>
         </div>
