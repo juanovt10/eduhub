86037ef8abbed93d2b16c5256ff882dbcd738b08
Juan Velasquez <juanovelasquez10@gmail.com>
2024-02-27 19:47:25 +0000

    Started developing filter functionality for courses page

--- src/pages/courses/CoursesPage.js
+++ src/pages/courses/CoursesPage.js
@@ -1,28 +1,104 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Container, Form, Row } from 'react-bootstrap'
+import { Button, Col, Container, Form, FormCheck, Row } from 'react-bootstrap'
 import { axiosReq } from '../../api/axiosDefaults';
 import CourseCard from './CourseCard';
-import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import axios from 'axios';
+import Rating from '../../components/Rating';
+
 
 const CoursesPage = () => {
     const [courses, setCourses] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
+    const [categories, setCategories] = useState({});
+    const [errors, setErrors] = useState({});
+    const [selectedCategories, setSelectedCategories] = useState({});
+    const [selectedPrice, setSelectedPrice] = useState('');
+    const [selectedResources, setSelectedResources] = useState({});
+    const [selectedRating, setSelectedRating] = useState(0);
+
+    const handleCategoryChange = (event) => {
+        const { value, checked } = event.target;
+
+        setSelectedCategories(prev => {
+            if (checked) {
+                return [...prev, value]
+            } else {
+                return prev.filter(category => category !== value)
+            }
+        })
+    };
+
+    const fetchCourses = async (filters = {}) => {
+        try {
+            let url = '/courses/';
+            const query = new URLSearchParams(filters).toString();
+            if (query) url += `?${query}`;
+
+            const {data} = await axiosReq.get(url);
+            setCourses(data);
+            console.log(data.results)
+            setHasLoaded(true);
+        } catch(err) {
+            console.log(err)
+        }
+    };
+    
+
 
     useEffect(() => {
-        const fetchCourses = async () => {
+        // const fetchCourses = async (filters = {}) => {
+        //     try {
+        //         let url = '/courses/';
+        //         const query = new URLSearchParams(filters).toString();
+        //         if (query) url += `?${query}`;
+
+        //         const {data} = await axiosReq.get('/courses/');
+        //         setCourses(data);
+        //         console.log(data.results)
+        //         setHasLoaded(true);
+        //     } catch(err) {
+        //         console.log(err)
+        //     }
+        // };
+        // const fetchCourses = async () => {
+        //     try {
+        //         const {data} = await axiosReq.get('/courses/');
+        //         setCourses(data);
+        //         console.log(data.results)
+        //         setHasLoaded(true);
+        //     } catch(err) {
+        //         console.log(err)
+        //     }
+        // };
+
+        const fetchCategories = async () => {
             try {
-                const {data} = await axiosReq.get('/courses/');
-                setCourses(data);
-                console.log(data.results)
-                setHasLoaded(true);
+                const response = await axios.get('https://eduhub-drf-api-8e84adf897cc.herokuapp.com/course-categories/');
+                setCategories(response.data);
             } catch(err) {
-                console.log(err)
+                setErrors(err.response?.data);
             }
-        }
+        };
 
         setHasLoaded(false);
         fetchCourses();
+        fetchCategories();
     }, []);
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const filters = {}
+
+        if (selectedCategories.length > 0) {
+            filters.categories = selectedCategories.join(',');
+        }
+
+        fetchCourses(filters);
+    }
 
     return (
         <div className='d-flex flex-column align-items-center justify-content-center'>
@@ -30,20 +106,29 @@ const CoursesPage = () => {
             <Container>
                 <Row>
                     <Col md={3}>
-                        <Accordion defaultActiveKey={['0']} alwaysOpen>
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>Accordion Item #1</Accordion.Header>
-                                <Accordion.Body>
-                                    Lorem ipsum dolor sit amet
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="1">
-                                <Accordion.Header>Accordion Item #2</Accordion.Header>
-                                <Accordion.Body>
-                                    Lorem ipsum dolor sit amet
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
+                        <Card>
+                            <Card.Header as="h5">Filters</Card.Header>
+                            <Form onSubmit={handleSubmit}>
+                                <Card.Body>
+                                    <Card.Title>Categories</Card.Title>
+                                    {Array.isArray(categories) && categories.map((cat, idx) => (
+                                        <Form.Check key={idx} value={cat.key} label={cat.value} onChange={handleCategoryChange}/>
+                                    ))}
+                                    <Card.Title>Resources</Card.Title>
+                                    <Form.Check label={<i class="fa-solid fa-video"></i>} />
+                                    <Form.Check label={<i class="fa-brands fa-readme"></i>} />
+                                    <Form.Check label={<i class="fa-solid fa-pen-to-square"></i>} />
+                                    <Card.Title>Ratings</Card.Title>
+                                    <Form.Check label={<Rating rating={5} />} />
+                                    <Form.Check label={<Rating rating={4} />} />
+                                    <Form.Check label={<Rating rating={3} />} />
+                                    <Form.Check label={<Rating rating={2} />} />
+                                    <Form.Check label={<Rating rating={1} />} />
+                                </Card.Body>
+                                <Button type="reset">Reset filters</Button>
+                                <Button type="submit">Apply filters</Button>
+                            </Form>
+                        </Card>
                     </Col>
                     <Col  md={9}>
                         {hasLoaded ? (
