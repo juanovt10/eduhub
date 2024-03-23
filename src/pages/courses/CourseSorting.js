5a36ce77ab4fac5dd5153d35a20201318bf8d633
Juan Velasquez <juanovelasquez10@gmail.com>
2024-03-23 16:56:25 +0000

    style sorting tabs in courses page

--- src/pages/courses/CourseSorting.js
+++ src/pages/courses/CourseSorting.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import styles from '../../styles/CourseSorting.module.css';
 
 
 const CourseSorting = ({ onSortingApplied }) => {
+    const [activeKey, setActiveKey] = useState("");
+
+    const handleSorting = (key) => {
+        setActiveKey(key);
+        onSortingApplied(key);
+    }
 
     return (
-        <Nav fill variant="tabs" defaultActiveKey="/home" className='mb-2'>
+        <Nav fill variant="tabs" defaultActiveKey="/home" className={`mb-2 ${styles.tabContainer}`}>
             <Nav.Item>
                 <Nav.Link disabled><i class="fa-solid fa-arrow-down"></i> <i class="fa-solid fa-arrow-up"></i> Sort By:</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={() => onSortingApplied('rating')}><i class="fa-solid fa-star"></i> Highly rated</Nav.Link>
+            <Nav.Item className={`${styles.tab} ${activeKey === 'rating' ? styles.activeLink : styles.inactiveLink}`} >
+                <Nav.Link 
+                    onClick={() => handleSorting('rating')}
+                    className={activeKey === 'rating' ? styles.activeLink : styles.inactiveLink}
+                    >
+                        <i class="fa-solid fa-star"></i> Highly rated
+                </Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={() => onSortingApplied('price')}><i class="fa-solid fa-hand-holding-dollar"></i> Lowest price</Nav.Link>
+            <Nav.Item className={activeKey === 'price' ? styles.activeLink : styles.inactiveLink}>
+                <Nav.Link
+                    onClick={() => handleSorting('price')}
+                    className={activeKey === 'price' ? styles.activeLink : styles.inactiveLink}
+                    >
+                        <i class="fa-solid fa-hand-holding-dollar"></i> Lowest price
+                </Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={() => onSortingApplied('creation')}><i class="fa-solid fa-clock"></i> Newest</Nav.Link>
+            <Nav.Item className={activeKey === 'creation' ? styles.activeLink : styles.inactiveLink}>
+                <Nav.Link 
+                    onClick={() => handleSorting('creation')}
+                    className={activeKey === 'creation' ? styles.activeLink : styles.inactiveLink}
+                    >
+                        <i class="fa-solid fa-clock"></i> Newest
+                </Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={() => onSortingApplied('enrollments')}><i class="fa-solid fa-graduation-cap"></i> Enrollments</Nav.Link>
+            <Nav.Item className={activeKey === 'enrollments' ? styles.activeLink : styles.inactiveLink}>
+                <Nav.Link 
+                    onClick={() => handleSorting('enrollments')}
+                    className={activeKey === 'enrollments' ? styles.activeLink : styles.inactiveLink}
+                >
+                    <i class="fa-solid fa-graduation-cap"></i> Enrollments
+                </Nav.Link>
             </Nav.Item>
         </Nav>
     )
--- src/pages/courses/CoursesPage.js
+++ src/pages/courses/CoursesPage.js
@@ -1,18 +1,15 @@
 import React, { useState } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
 import CoursesDisplay from './CoursesDisplay';
 import CourseFilter from './CourseFilter';
 import CourseSorting from './CourseSorting';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import styles from '../../styles/CoursesPage.module.css';
-import { useCurrentUser } from '../../context/CurrentUserContext';
 
 
 const CoursesPage = () => {
     const [sortKey, setSortKey] = useState('default');
     const [filters, setFilters] = useState({});
-    const currentUser = useCurrentUser();
-
-    console.log(currentUser)
     
     const handleFiltersApplied = (newFilters) => {
         setFilters(newFilters);
@@ -23,8 +20,8 @@ const CoursesPage = () => {
     }
 
     return (
-        <div className={`d-flex flex-column align-items-center justify-content-center`}>
-            <h1 className={`text-center ${styles.pageTitle}`}>Explore our courses</h1>
+        <div className={styles.pageContainer}>
+            <h1 className={styles.pageTitle}>Our courses</h1>
             <div className='mx-5'>
                 <Row>
                     <Col sm={4} md={3} className='d-none d-sm-block'>
